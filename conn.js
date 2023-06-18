9dbc2de07cd8711135fe7817a80ed01857048100
Talita Gabriela Del Poso Vilalva <talitagabriela1216@gmail.com>
2023-06-17 21:13:53 -0400

    COMMIT

--- conn.js
+++ conn.js
@@ -1,11 +1,11 @@
 const { MongoClient } = require('mongodb');
 
-const connectionString = 'mongodb://localhost/127.0.0.1:27017';
+const connectionString = 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.0';
 const client = new MongoClient(connectionString);
 
 try {
   let db = client.db("jogo");
   module.exports = db;
-} catch(e) {
-  console.error(e);
+} catch(error) {
+  console.error(error);
 }
---
+++ meu_script.js
@@ -0,0 +1,87 @@
+const numero_moedas = 108;
+const tempo_inicial = 30;
+let pontos = 0;
+let tempo = 0;
+let timer = null;
+const name = prompt("Qual seu nome?", "Escreva");
+
+function criarElemento(nome, pontos){
+  const container = document.getElementById('melhoresPontuadores');
+  nome = document.createElement('p');
+  pontos = document.createElement('span');
+
+  nome.textContent = name;
+  pontos.textContent = pontos(name);
+
+  container.appendChild(nome);
+  container.appendChild(pontos);
+}
+
+function iniciaJogo(){
+pontos = 0;
+tempo = tempo_inicial;
+let tela = document.getElementById("tela");
+tela.innerHTML = "";
+
+for(let i = 0; i < numero_moedas; ++i){
+let moeda = document.createElement("img");
+moeda.src = "doce.png";
+moeda.id = "j" + i;
+moeda.onclick = function(){
+pegaMoeda(this);
+}
+tela.appendChild(moeda);
+}
+timer = setInterval(contaTempo,1000);
+}
+
+fetch('http://localhost:5050/score')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Erro na requisição'); 
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data)
+    })
+    .catch(error => {
+      console.log(error);
+    });
+
+function pegaMoeda(moeda){
+moeda
+moeda.src = "delicia.png";
+++pontos;
+let contadorPontos = document.getElementById("pontos");
+contadorPontos.innerText = pontos;
+}
+function contaTempo(){
+if(tempo > 0){
+--tempo;
+let contadorTempo = document.getElementById("tempo");
+contadorTempo.innerText = tempo;
+
+return contaTempo = null;
+}
+
+if(tempo <= 0){
+clearInterval(timer);
+alert("você fez"+ " " + pontos + " " + "pontos, parabéns!");
+iniciaJogo();
+}
+}
+
+let pontuacao = {
+    name: name,
+    pontos: pontos
+  }
+
+  fetch('http://localhost:5050/score', {
+    method: "POST",
+    body: JSON.stringify(pontuacao),
+    headers: {"Content-type":"application/json; charset=UTF-8"}
+  })
+  .then(response => response.json())
+  .then(json => console.log(json))
+  .catch(error => console.log(error))
